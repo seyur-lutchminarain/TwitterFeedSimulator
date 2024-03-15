9023be69f7b6c8f12923a9761c27ca348c2bb92b
Seyur Lutchminarain <slutchminarain@oldmutual.com>
2024-03-15 17:57:36 +0200

    Project changes

--- Program.cs
+++ Program.cs
@@ -17,8 +17,12 @@ namespace TwitterFeedSimulator
 
             // Instantiate instance of TwitterFeedSimulatorClass
             var _twitterFeedSimulator = new TwitterFeedSimulatorService();
+            
+            // If you are using VS Code or any other IDE use this
+            const string INPUT_DATA_ROOT = @"Domain/Core/Stubs";
 
-            const string INPUT_DATA_ROOT = @"../../../Domain/Core/Stubs";
+            // If you are using visual studio specifically use this
+            const string VS_INPUT_DATA_ROOT = @"../../../Domain/Core/Stubs"
 
             string userFilePath = INPUT_DATA_ROOT + "/user.txt";
             string tweetFilePath = INPUT_DATA_ROOT + "/tweet.txt";
